f9ff8efa882920b7d72d74235d89c88f3c375e61
dylandoamaral <do.amaral.dylan@gmail.com>
2022-03-15 21:34:50 +0100

    chore: fix release

--- release.sh
+++ release.sh
@@ -6,8 +6,8 @@ if [ -n "$1" ]; then
         read -r -p "Are you sure to upgrade to v$1 ? [y/N] " response
         if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]
         then
-            # update the version in const.ts
-            sed "s/^  \"version\": \".*$/  \"version\": \"${1#v}\",/" -i custom_components/trakt_tv/manifest.json
+            # update the version in manifest.json
+            sed -i '' -E "s|\"version\": \".*\"|\"version\": \"${1#v}\"|" custom_components/trakt_tv/manifest.json
             git add custom_components/trakt_tv/manifest.json
             git commit -m "chore(release): prepare for v$1"
             git tag "v$1"
