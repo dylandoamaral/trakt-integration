cb6c72c2f89ad174f78c90a9c1d11ed1023deff8
Matt Olson <SirIndubitable@gmail.com>
2026-01-06 17:35:19 +0100

    Fix removal of progress_watched_reset

--- custom_components/trakt_tv/apis/trakt.py
+++ custom_components/trakt_tv/apis/trakt.py
@@ -143,8 +143,8 @@ class TraktApi:
             for section in [
                 "calendar",
                 "progress_watched",
+                "progress_watched_reset",
                 "progress_collected",
-                "recommendations",
                 "dropped",
             ]:
                 hidden_items = await self.request(
